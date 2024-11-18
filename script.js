7ae0b5719fbb27387d861a28decf69eb290f6c79
Sascha-La <al.artishevskaya98@gmail.com>
2024-11-18 22:39:24 +0300

    add isRunning

--- script.js
+++ script.js
@@ -3,24 +3,34 @@ const currentTime = document.getElementById('pomodoro-time');
 let startMinutes = 1500;
 
 const startButton = document.querySelector('#start');
-const stopButton = document.querySelector('#start');
+var isRunning;
 
-const timeId = startButton.addEventListener('click', function countTime() {
+const timeCounter = startButton.addEventListener('click', function countTime() {
     const minutes = Math.floor(startMinutes / 60).toString().padStart(2, '0');
     const seconds = (startMinutes % 60).toString().padStart(2, '0');
     currentTime.textContent = `${minutes}:${seconds}`;
 
-    if (startMinutes > 0) {
-        startMinutes--;
-        setTimeout(countTime, 1000);
-    }
 
-    if (startButton) {
+    if (isRunning) {
+        isRunning = clearInterval(isRunning);
+        startButton.textContent = "start";
+    } else {
+        isRunning = setInterval(countTime, 1000);
         startButton.textContent = "stop";
     }
 
+
+
+    if (startMinutes > 0) {
+        startMinutes--;
+    }
+
+    // if (startButton) {
+    //      startButton.textContent = "stop";
+    //   }
+
 })
 
-startButton.addEventListener('click', function stopTime() {
-    clearTimeout(timeId);
-})
+//startButton.addEventListener('click', function stopTime() {
+//   clearTimeout(countTime);
+//})
